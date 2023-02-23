a758ffcf822b0fb6e887335f956aee7774f39c45
Essam Afzal <essam.afzal@outlook.com>
2023-02-23 18:05:35 +0400

    added functional corsWhitelist

--- server.js
+++ server.js
@@ -32,10 +32,7 @@ app.use(function (req, res, next) {
   ];
 
   if (corsWhiteList.includes(req.headers.origin)) {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://aurora-jewelry-frontend.onrender.com"
-    );
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     next();
   } else {
     return res.status(403).send("Access denied");
